1d95930ded9cedea422a9362efa0fe0874643672
cdg0413 <whehdrjs0207@gmail.com>
2025-05-13 13:40:24 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div>
         <!-- Vue + Vite Template -->
         <h1 style="text-align: center; font-size: 80px">APP</h1>
+        <div>테스트예정</div>
         <router-view></router-view>
     </div>
 </template>
