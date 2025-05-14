8a34708c6da1d50004b8f4bf66b1c063390f7836
cdg0413 <whehdrjs0207@gmail.com>
2025-05-14 15:20:30 +0900

    naver map 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="mapWrap">
-        <div class="mapArea"></div>
+        <div class="mapArea">
+            <div id="map" style="width: 100%; height: 100%"></div>
+        </div>
     </div>
 </template>
 <script>
@@ -10,9 +12,26 @@ import { supabase } from '@/api/supabase';
 export default {
     name: 'Home',
     data() {
-        return {};
+        return {
+            map: null,
+            marker: null,
+        };
     },
     mounted() {
+        // https://navermaps.github.io/maps.js.ncp/docs/tutorial-2-Getting-Started.html
+
+        // 지도 초기화
+        this.map = new naver.maps.Map('map', {
+            center: new naver.maps.LatLng(37.5665, 126.978),
+            zoom: 14,
+        });
+
+        //최초 마커 생성
+        // this.marker = new naver.maps.Marker({
+        //     position: new naver.maps.LatLng(37.5665, 126.978),
+        //     map: this.map,
+        // });
+
         this.getList();
     },
     methods: {
@@ -26,6 +45,12 @@ export default {
 
             console.log('data: ', data);
         },
+
+        // changeMarker(lat, lng) {
+        //     const newPosition = new naver.maps.LatLng(lat, lng);
+        //     this.marker.setPosition(newPosition); // 마커 위치만 변경
+        //     this.map.setCenter(newPosition); // 지도 중심도 같이 이동
+        // },
     },
 };
 </script>
